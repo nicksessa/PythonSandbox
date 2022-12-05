afc63c103977b317026799430241fd16fa9437ea
NickSessa <nicholas.sessa@gmail.com>
2022-12-04 21:46:46 -0500

    Made updates to mod6

--- ModuleSixMilestone.py
+++ ModuleSixMilestone.py
@@ -10,8 +10,8 @@
 #
 #
 # TODO: Complete the main project.
-# TODO: Validate and restrict input.
 
+import string
 
 # A dictionary for the simplified dragon text game
 # The dictionary links a room to other rooms.
@@ -23,34 +23,72 @@ rooms = {
 
 currentLocation = 'Great Hall'
 
+def welcome():
+    print('***************************')
+    print('* Welcome to the Dungeon! *')
+    print('***************************')
 
 def status():  # player's status
-    print('You are in the {}.'.format(currentLocation))
+    print('\nYou are in the {}.'.format(currentLocation))
     print('Inventory : []')
     print('------------------')
     print('Enter your move: ', end='')
     return ''
 
+def help():
+    print('\n---------------------HELP---------------------')
+    print('| To move, type "go north", "go south", etc. |')
+    print('| To exit, type "exit" or "q".               |')
+    print('| Type "help" to see this message again.     |')
+    print('----------------------------------------------')
+    return ''
+
 exit_dungeon = False
 
+welcome()
+help()
+
 while not exit_dungeon:
-    player_input = input(status()).lower().strip()
-    command = player_input.split()
-    if command[0] == 'exit':
+    player_input = input(status()).lower().strip()  # Get input from the player.
+    command = player_input.split()                  # Put the input into a list.
+    # print('len command:', len(command))
+    if not command:                                 # Check to see if the player input anything.
+        print('--- Invalid command! ---')           # If not, continue.
+        continue
+    if command[0] == 'exit' or command[0] == 'q':   # If player types 'exit', then exit the game.
         exit_dungeon = True
         currentLocation = 'exit'
-    elif command[0] != 'go':
-        print('Invalid command!')
+        continue
+    if command[0] == 'help':
+        help()
+    elif len(command) < 2:                          # If less than two words were entered, continue.
+        print('--- Invalid command! ---')
+        continue
+    elif command[0] != 'go':                        # Check if the first string is 'go'. If not, continue.
+        print('--- Invalid command! ---')
         continue
     else:
-        direction = command[1]
+        direction = command[1]                      # The second string is the direction.
+        direction = direction.capitalize()          # Capitalize it to match the key in the dictionary.
+
+        # Remove punctuation from the direction:
+        direction = direction.translate(str.maketrans('', '', string.punctuation))
+
+        # print('Direction:', direction)
         # look up the current location in the rooms dictionary and get valid exit points.
-        if currentLocation in rooms:
-            exit_points = rooms[currentLocation]
-            print('Exit points:', exit_points)
+        if currentLocation in rooms:                # If location is in the rooms dict...
+            exit_points = rooms[currentLocation]    # get the exit points.
+            # print('Exit points:', exit_points)
+            if direction in exit_points:   # If the direction is available, then change to that location.
+                # print('Moving to the', exit_points[direction])
+                currentLocation = exit_points[direction]
+            else:
+                print('\n*** You can\'t go that way! ***')    # If the direction is not there, warn user and continue.
+                continue
         else:
-            print('Invalid room')
+            print('--- Invalid room ---')               # I don't think this is possible
+                                                        # but it can't hurt to cover all the bases.
 
 
 if currentLocation == 'exit':
-    print('You have left the dungeon!')
+    print('\nYou have left the dungeon!')
